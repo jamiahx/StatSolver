6567255f22c62f2032463bac5c67a5e78160546e
jamiahx <jamiahx@gmail.com>
2013-02-21 14:04:40 -0600

    Update StatSolver.py

--- StatSolver.py
+++ StatSolver.py
@@ -1,1 +1,13 @@
 # main program file
+# pseudocode
+print "Hello, and welcome to StatSolver!"
+print "Which program would you like to run today?"
+print "1. Set-Solver"
+input program_select
+if program_select == 1
+	import libsetfunctions.py
+	reset_vars
+	set_vars
+	print vars_unknown" 21.review 22.solve 23.delete_var 24.reset 25.quit Set-Solver 26.quit StatSolver"
+	input do
+	
